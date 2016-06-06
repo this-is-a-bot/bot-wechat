9794d8290d0674ecd3dec4ad2e90159afd261edf
Junjia He <edfward@gmail.com>
2016-06-06 06:06:30 +0000

    Add dummy response msg

--- server.php
+++ server.php
@@ -26,6 +26,10 @@ $options = [
 
 $app = new Application($options);
 
+$server->setMessageHandler(function ($message) {
+    return "您好！欢迎关注我!";
+});
+
 $response = $app->server->serve();
 
 // 将响应输出
