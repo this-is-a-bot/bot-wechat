4632d8c3818152444b89d1e6e133cc1c79aaa4f4
Junjia He <edfward@gmail.com>
2016-07-10 00:59:16 -0700

    Support fetching featured games (#3)

--- src/Steam/Steam.php
+++ src/Steam/Steam.php
@@ -1,5 +1,4 @@
-<?php
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace BotWechat\Steam;
 
@@ -8,21 +7,40 @@ use EasyWeChat\Message\News;
 
 class Steam {
 
+    const URL = 'http://a-b0t.herokuapp.com';
+
     public static function getSteamDiscounts(): array {
-        $url = 'http://a-b0t.herokuapp.com/steam/discounts';
-        $raw_response = file_get_contents($url);
+        $endpoint = self::URL . '/steam/discounts';
+        $raw_response = file_get_contents($endpoint);
         $games = json_decode($raw_response, true);
-        $responses = array();
+        $responses = [];
         foreach ($games as &$game) {
-            $new = new News([
+            array_push($responses, self::wrapGame($game));
+        };
+        return array_slice($responses, 0, 5);
+    }
+
+    public static function getSteamFeaturedGames(string $feature): array {
+        $endpoint = self::URL . '/steam/featured';
+        if ($feature) {
+            $endpoint .= '?feature=' . $feature;
+        }
+        $raw_response = file_get_contents($endpoint);
+        $games = json_decode($raw_response, true);
+        $responses = [];
+        foreach ($games as &$game) {
+            array_push($responses, self::wrapGame($game));
+        };
+        return array_slice($responses, 0, 5);
+    }
+
+    private static function wrapGame(array $game) {
+        return new News([
                 'title'       => $game['name'] . '($'. $game['priceBefore'] . '->$' . $game['priceNow'] . ')',
                 'description' => $game['review'],
                 'url'         => $game['url'],
                 'image'       => $game['imgSrc']
-            ]);
-            array_push($responses, $new);
-        };
-        return array_slice($responses, 0, 5);
+        ]);
     }
 
 }
